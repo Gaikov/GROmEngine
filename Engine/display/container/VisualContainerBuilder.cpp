f31a759299b4c770f5540e40ca0e021025c4a3ed
Roman.Gaikov <gaikov@inbox.ru>
2024-10-21 16:42:03 +0300

    container builder: masking ability added

--- Engine/display/container/VisualContainerBuilder.cpp
+++ Engine/display/container/VisualContainerBuilder.cpp
@@ -5,6 +5,7 @@
 #include "VisualContainerBuilder.h"
 #include "Engine/display/container/VisualContainer2d.h"
 #include "nsLib/log.h"
+#include "Core/ParserUtils.h"
 
 nsVisualObject2d *nsVisualContainerBuilder::CreateDefault() {
     return new nsVisualContainer2d();
@@ -15,7 +16,7 @@ bool nsVisualContainerBuilder::Parse(script_state_t *ss, nsVisualObject2d *objec
         return false;
     }
 
-    auto container = dynamic_cast<nsVisualContainer2d*>(object);
+    auto container = Cast<nsVisualContainer2d>(object);
     if (!container) {
         return false;
     }
@@ -30,6 +31,25 @@ bool nsVisualContainerBuilder::Parse(script_state_t *ss, nsVisualObject2d *objec
             } while (ps_block_next(ss));
             ps_block_end(ss);
         }
+        ps_block_end(ss);
+    }
+
+    if (ps_block_begin(ss, "masking")) {
+        do {
+            auto maskId = ParseString(ss, "mask");
+            auto mask = dynamic_cast<nsVisualMask*>(container->GetChildById(maskId));
+            if (mask) {
+                auto maskedId = ParseString(ss, "masked");
+                auto masked = container->GetChildById(maskedId);
+                if (masked) {
+                    masked->AddMask(mask);
+                } else {
+                    Log::Warning("Invalid masked object: %s", maskedId.AsChar());
+                }
+            } else {
+                Log::Warning("invalid mask object: %s", maskId.AsChar());
+            }
+        } while (ps_block_next(ss));
         ps_block_end(ss);
     }
 
