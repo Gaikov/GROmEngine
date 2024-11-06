44d44ee2b27db0c7dfc9a1878d7d99df13ed44df
Roman.Gaikov <gaikov@inbox.ru>
2024-11-06 12:09:55 +0300

    circle to rectangle collision (in progress)

--- TestsApp/tests/collision/CollisionTest.h
+++ TestsApp/tests/collision/CollisionTest.h
@@ -8,20 +8,27 @@
 #include "Engine/test/BaseFunctionalTest.h"
 #include "Engine/display/container/VisualContainer2d.h"
 #include "Engine/display/graphics/VisualRect.h"
+#include "Engine/display/graphics/VisualCircle.h"
 
 class nsCollisionTest : public BaseFunctionalTest {
 public:
+    nsCollisionTest();
+
     bool Init() override;
     void Release() override;
     void Loop(float deltaTime) override;
     void Draw() override;
 
     bool OnPointerMove(float x, float y, int pointerId) override;
+    void OnKeyDown(int key, bool rept) override;
 
 private:
     nsVisualContainer2d *_stage = nullptr;
     nsVisualRect        *_rect = nullptr;
     nsVisualRect        *_userRect = nullptr;
+    nsVisualCircle      *_userCircle = nullptr;
+
+    nsProperty<bool>    _rectColliding;
 
     nsVisualRect *CreateRect(float width, float height);
 };
