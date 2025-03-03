60b1b5cfe1b8808e9b5892adacd7dd22cc006c86
Roman.Gaikov <gaikov@inbox.ru>
2025-03-03 12:27:29 +0300

    ImageButton: labels support

--- Engine/display/button/ImageButtonBuilder.cpp
+++ Engine/display/button/ImageButtonBuilder.cpp
@@ -6,6 +6,7 @@
 #include "ImageButton.h"
 #include "Core/ParserUtils.h"
 #include "RenManager.h"
+#include "renderer/font/FontsCache.h"
 
 nsVisualObject2d *nsImageButtonBuilder::CreateDefault() {
     return new nsImageButton();
@@ -21,6 +22,12 @@ bool nsImageButtonBuilder::Parse(script_state_t *ss, nsVisualObject2d *o, nsVisu
     ParseState(ss, "over", button->over);
     ParseState(ss, "down", button->down);
     ParseState(ss, "disabled", button->disabled);
+
+    if (ps_block_begin(ss, "label")) {
+        button->text = ParseStrP(ss, "text");
+        button->font = nsFontsCache::Shared()->LoadFont(ParseString(ss, "font"));
+        ps_block_end(ss);
+    }
 
     return nsVisualBuilder2d::Parse(ss, o, context);
 }
