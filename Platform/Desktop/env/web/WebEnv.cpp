bdac814c0784f69bb72ad9c4d8b82170cf760b7a
Roman.Gaikov <gaikov@inbox.ru>
2025-04-17 13:01:31 +0300

    - wasm support (in porgress)

--- Platform/Desktop/env/web/WebEnv.cpp
+++ Platform/Desktop/env/web/WebEnv.cpp
@@ -4,7 +4,14 @@
 
 #include "WebEnv.h"
 #include "WebLog.h"
+#include "Engine/engine.h"
+#include <emscripten/emscripten.h>
 #include <emscripten/console.h>
+
+static void Loop() {
+	nsEngine::MainLoop();
+	glfwPollEvents();
+}
 
 bool nsWebEnv::Init() {
     Log::Init();
@@ -16,4 +23,8 @@ void nsWebEnv::MessagePopup(const char *title, const char *message) {
 }
 
 void nsWebEnv::OpenUrl(const char *url) {
+}
+
+void nsWebEnv::MainLoop(GLFWwindow *wnd) {
+	emscripten_set_main_loop(Loop, 0, true);
 }
