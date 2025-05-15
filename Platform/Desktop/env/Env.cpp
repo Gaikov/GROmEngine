cc4fc82b5a336974e767ce05eb8e9dd5f969412f
Roman.Gaikov <gaikov@inbox.ru>
2025-05-15 18:51:44 +0300

    - multi touch support for wasm

--- Platform/Desktop/env/Env.cpp
+++ Platform/Desktop/env/Env.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "Env.h"
+#include "Engine/engine.h"
 
+static std::vector<int> mouseButtons;
 nsEnv* nsEnv::_shared = nullptr;
 
 bool nsEnv::Create() {
@@ -18,3 +20,36 @@ void nsEnv::Destroy() {
     delete _shared;
     _shared = nullptr;
 }
+
+void nsEnv::PrepareDesktopInput() {
+    glfwSetScrollCallback(_wnd, [](GLFWwindow *window, double x, double y) -> void {
+        auto ui = nsEngine::GetActiveInput();
+        if (ui) ui->OnMouseWheel((float) y);
+    });
+
+    glfwSetCursorPosCallback(_wnd, [](GLFWwindow *window, double xpos, double ypos) -> void {
+        if (!mouseButtons.empty()) {
+            for (auto button: mouseButtons) {
+                nsEngine::OnPointerMove(button, (int) xpos, (int) ypos);
+            }
+        } else {
+            nsEngine::OnPointerMove(-1, (int) xpos, (int) ypos);
+        }
+    });
+
+    glfwSetMouseButtonCallback(_wnd, [](GLFWwindow *window, int button, int action, int mods) -> void {
+        double x, y;
+        glfwGetCursorPos(window, &x, &y);
+        if (action == GLFW_PRESS) {
+            mouseButtons.push_back(button);
+            nsEngine::OnPointerDown(button, (int) x, (int) y);
+        } else if (action == GLFW_RELEASE) {
+            auto it = std::remove(mouseButtons.begin(), mouseButtons.end(),button);
+            if (it != mouseButtons.end()) {
+                mouseButtons.erase(it);
+            }
+            nsEngine::OnPointerUp(button, (int) x, (int) y);
+        }
+    });
+}
+
