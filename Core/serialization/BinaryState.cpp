f42da2f8a1993462c8a05faf784361fec2992424
Roman Gaikov <gaikov@inbox.ru>
2025-07-08 00:01:12 +0300

    Add `BinaryState` and `BinaryVar` serialization framework
    Introduced `BinaryState` for managing serialized variables and `BinaryVar` as a template-based abstraction for variable serialization and deserialization. Updated CMakeLists to include new serialization components.

---
+++ Core/serialization/BinaryState.cpp
@@ -0,0 +1,29 @@
+//
+// Created by roman on 7/7/25.
+//
+
+#include "BinaryState.h"
+#include "memory/FastMemManager.h"
+
+void nsBinaryState::RegisterVar(IBinaryVar *var) {
+   assert(var->next == nullptr);
+   var->next = _vars;
+   _vars = var;
+   _bufferSize += var->GetSize();
+}
+
+void nsBinaryState::Serialize(void *buffer) const {
+   auto data = static_cast<int8_t *>(buffer);
+   for (auto var = _vars; var; var = var->next) {
+      var->Serialize(data);
+      data += var->GetSize();
+   }
+}
+
+void nsBinaryState::Deserialize(const void *buffer) const {
+   auto data = static_cast<const int8_t *>(buffer);
+   for (auto var = _vars; var; var = var->next) {
+      var->Deserialize(data);
+      data += var->GetSize();
+   }
+}
