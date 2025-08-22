4ce0a3a02c9681cd71fddc6670d27345dc244064
Roman.Gaikov <gaikov@inbox.ru>
2025-08-22 16:21:28 +0300

    Refactor `ClientConnection` and `Server` to improve connection handling and encapsulate data sending logic
    - Introduced `SendData` in `nsClientConnection` to replace `Send` and handle disconnected states gracefully.
    - Refactored `_connected` state management with `std::atomic` for thread safety.
    - Updated `Server`'s packet broadcasting and sending methods to use `SendData` for consistency.

--- Networking/server/ClientConnection.h
+++ Networking/server/ClientConnection.h
@@ -16,17 +16,21 @@ public:
     virtual void OnClientDisconnect(nsClientConnection *connection) = 0;
 };
 
-class nsClientConnection : public nsBaseClientSocket {
+class nsClientConnection : protected nsBaseClientSocket {
 public:
     nsClientConnection(int socket, int id, nsClientConnectionContext *context);
     ~nsClientConnection() override;
     void Disconnect();
     [[nodiscard]] int32_t GetClientId() const { return _clientId; }
 
+    bool SendData(const void *data, unsigned int size) const;
+
+
 private:
     int32_t         _clientId;
 
     std::thread _thread;
+    std::atomic<bool> _connected = true;
 
     void WaitThread();
 };
