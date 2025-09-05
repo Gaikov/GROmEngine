e6dffe689808db7ac7488c316473e1b5258be48a
Roman.Gaikov <gaikov@inbox.ru>
2025-09-05 13:49:43 +0300

    Add overload to `DrawLine` in `nsDebugDrawInfo` for position-based drawing
    - Introduced new `DrawLine` method accepting position, text, and color to enhance drawing flexibility.
    - Refactored existing `DrawLine` implementation to utilize the new method for code reuse.

--- Engine/utils/DebugDrawInfo.cpp
+++ Engine/utils/DebugDrawInfo.cpp
@@ -11,9 +11,14 @@ nsDebugDrawInfo::nsDebugDrawInfo(const nsVec2 &startPos) : _pos(startPos) {
 }
 
 void nsDebugDrawInfo::DrawLine(const char *text, const nsColor &c) {
-    auto font = nsFontsCache::Shared()->SysFont();
-    nsVec2  scale = {0.5, 1};
-    font->Draw(text, _pos, scale, c);
+    DrawLine(_pos, text, c);
 
+    const auto font = nsFontsCache::Shared()->SysFont();
     _pos.y += font->GetLineHeight() + 2;
+}
+
+void nsDebugDrawInfo::DrawLine(const nsVec2 &pos, const char *text, const nsColor &c) {
+    const auto font = nsFontsCache::Shared()->SysFont();
+    const nsVec2  scale = {0.5, 1};
+    font->Draw(text, pos, scale, c);
 }
