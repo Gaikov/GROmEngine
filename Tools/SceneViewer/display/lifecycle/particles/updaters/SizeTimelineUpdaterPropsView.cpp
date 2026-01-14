aec2a2ee9ad0274b6b9a1fd4d94c5b81b3f0d009
Roman.Gaikov <gaikov@inbox.ru>
2026-01-14 17:27:42 +0300

    Add undo support for SizeTimelineUpdaterPropsView point manipulation
    Implemented undo functionality for point dragging in `SizeTimelineUpdaterPropsView`. Introduced `UndoVarPrevChange` to track and revert changes to timeline frames. Enhanced point dragging with drag start/stop handling for robust undo state management.

--- Tools/SceneViewer/display/lifecycle/particles/updaters/SizeTimelineUpdaterPropsView.cpp
+++ Tools/SceneViewer/display/lifecycle/particles/updaters/SizeTimelineUpdaterPropsView.cpp
@@ -4,6 +4,9 @@
 
 #include "SizeTimelineUpdaterPropsView.h"
 
+#include "Core/undo/UndoBatch.h"
+#include "Core/undo/UndoService.h"
+#include "Core/undo/UndoVarPrevChange.h"
 #include "Engine/renderer/particles/updater/ParticlesSizeUpdater.h"
 #include "Engine/renderer/particles/updater/size/ParticlesSizeTimelineUpdater.h"
 #include "imgui/implot.h"
@@ -20,41 +23,49 @@ void nsSizeTimelineUpdaterPropsView::Draw(nsParticlesUpdater *object, nsPropsCon
     const auto u = dynamic_cast<nsParticlesSizeTimelineUpdater *>(object);
 
     if (ImPlot::BeginPlot("Scale Curve")) {
+        int draggedPoint = -1;
         ImPlot::SetupAxesLimits(0, 1, 0, 2);
 
         const auto size = u->timeline.size();
         const auto &points = u->timeline;
         if (size > 1) {
-            xs.resize(size);
-            ys.resize(size);
+            _xs.resize(size);
+            _ys.resize(size);
             float x = 0;
             for (int i = 0; i < size; ++i) {
                 x += points[i]->time;
-                xs[i] = x;
-                ys[i] = points[i]->data;
+                _xs[i] = x;
+                _ys[i] = points[i]->data;
             }
-            ImPlot::PlotLine("Scale", xs.data(), ys.data(), static_cast<int>(size));
+            ImPlot::PlotLine("Scale", _xs.data(), _ys.data(), static_cast<int>(size));
         }
 
         float prev, next;
         for (int i = 0; i < size; ++i) {
             if (i == 0) {
                 prev = 0;
-                next = xs[1];
+                next = _xs[1];
             } else if (i == size - 1) {
-                prev = xs[i - 1];
+                prev = _xs[i - 1];
                 next = 1;
             } else {
-                prev = xs[i - 1];
-                next = xs[i + 1];
+                prev = _xs[i - 1];
+                next = _xs[i + 1];
             }
 
-            double x = xs[i];
-            double y = ys[i];
+            double x = _xs[i];
+            double y = _ys[i];
 
             const auto color = _selectedPoint == i ? SELECTED_POINT_COLOR : DEFAULT_POINT_COLOR;
 
             if (ImPlot::DragPoint(i, &x, &y, color, POINT_SIZE)) {
+                draggedPoint = i;
+                _selectedPoint = i;
+                if (_draggingPoint != i) {
+                    _draggingPoint = i;
+                    OnDragStarted(u);
+                }
+
                 x = nsMath::Clamp(static_cast<float>(x), prev, next);
                 const auto time = static_cast<float>(x - prev);
                 const auto timeNext = static_cast<float>(next - x);
@@ -69,8 +80,6 @@ void nsSizeTimelineUpdaterPropsView::Draw(nsParticlesUpdater *object, nsPropsCon
                 if (i && i + 1 < size) {
                     points[i + 1]->time = timeNext;
                 }
-
-                _selectedPoint = i;
             }
 
             if (ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
@@ -85,6 +94,34 @@ void nsSizeTimelineUpdaterPropsView::Draw(nsParticlesUpdater *object, nsPropsCon
             }
         }
 
+        if (_draggingPoint != -1 && _draggingPoint != draggedPoint) {
+            _draggingPoint = -1;
+            OnDragStopped(u);
+        }
+
         ImPlot::EndPlot();
     }
+}
+
+void nsSizeTimelineUpdaterPropsView::OnDragStarted(const nsParticlesSizeTimelineUpdater *object) {
+    Log::Info("Drag started");
+
+    _prevFrames.clear();
+    for (auto &f : object->timeline) {
+        _prevFrames.push_back(*f);
+    }
+}
+
+void nsSizeTimelineUpdaterPropsView::OnDragStopped(const nsParticlesSizeTimelineUpdater *object) const {
+    Log::Info("Drag stopped");
+
+    const auto batch = new nsUndoBatch();
+    for (int i = 0; i < _prevFrames.size(); ++i) {
+        const auto &frame = object->timeline[i];
+        const auto &prevValue = _prevFrames[i];
+
+        batch->Add(new nsUndoVarPrevChange(frame->time, prevValue.time));
+        batch->Add(new nsUndoVarPrevChange(frame->data, prevValue.data));
+    }
+    nsUndoService::Shared()->Push(batch);
 }
--- Tools/SceneViewer/display/lifecycle/particles/updaters/SizeTimelineUpdaterPropsView.h
+++ Tools/SceneViewer/display/lifecycle/particles/updaters/SizeTimelineUpdaterPropsView.h
@@ -5,17 +5,24 @@
 //--------------------------------------------------------------------------------------------------
 #pragma once
 #include "UpdaterPropsRegistry.h"
+#include "Engine/renderer/particles/updater/size/ParticlesSizeTimelineUpdater.h"
 
 class nsSizeTimelineUpdaterPropsView final : public nsUpdaterPropsView {
 public:
     bool IsSupported(nsParticlesUpdater *object) override;
+
     void Draw(nsParticlesUpdater *object, nsPropsContext *context) override;
 
 private:
-    std::vector<float> xs;
-    std::vector<float> ys;
+    std::vector<float> _xs;
+    std::vector<float> _ys;
+
+    std::vector<nsParticlesSizeTimelineUpdater::Frame> _prevFrames;
 
     int _selectedPoint = -1;
     int _draggingPoint = -1;
+
+    void OnDragStarted(const nsParticlesSizeTimelineUpdater *object);
+    void OnDragStopped(const nsParticlesSizeTimelineUpdater *object) const;
 
 };
