97a1055158d49c92aa3ac2cb61769c1db237e5af
Roman.Gaikov <gaikov@inbox.ru>
2026-01-21 18:08:34 +0300

    Update LibraryView to improve layout organization and usability
    Renamed "Assets Library" to "Scene Layouts" for clarity. Organized layout elements into a collapsible "Layouts Library" section. Enhanced child window handling with border and resize functionality.

--- Tools/SceneViewer/view/library/LibraryView.cpp
+++ Tools/SceneViewer/view/library/LibraryView.cpp
@@ -22,53 +22,59 @@ nsLibraryView::nsLibraryView() {
 }
 
 void nsLibraryView::Draw() {
-    ImGui::Begin("Assets Library");
+    ImGui::Begin("Scene Layouts");
 
     nsCompositeView::Draw();
 
-    ImGui::InputText("Search", _filter.AsChar(), nsString::MAX_SIZE - 1);
-    ImGui::SameLine();
-    if (ImGui::Button("Clear")) {
-        _filter = "";
-    }
-
-    auto &user = _model->project.user;
-
-    ImGui::BeginChild("LayoutsLib", ImVec2(0, 300), true, ImGuiWindowFlags_HorizontalScrollbar);
-    for (auto file: _model->project.scenes.GetFiles()) {
-        if (_filter.IsEmpty() || strstr(file.AsChar(), _filter.AsChar())) {
-            if (ImGui::Selectable(file.AsChar(), user.currentScene == file.AsChar(), ImGuiSelectableFlags_AllowDoubleClick)) {
-                if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
-                    Log::Info("Selected: %s", file.AsChar());
-                    const std::string value = file.AsChar();
-                    const auto undo = nsUndoService::Shared();
-                    const auto batch = new nsUndoBatch();
-
-                    batch->Add(new nsUndoVarChange(user.selectedObject, static_cast<nsVisualObject2d *>(nullptr)));
-                    batch->Add(new nsUndoVarChange(user.currentScene, value));
-
-                    undo->Push(batch);
-                }
-            }
-
-            if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
-                const char* data = file.AsChar();
-                ImGui::SetDragDropPayload("MY_DND_TYPE", data, strlen(data) + 1);
-                ImGui::Text("Drop to scene tree...");
-                ImGui::EndDragDropSource();
-            }//*/
+    if (ImGui::CollapsingHeader("Layouts Library", ImGuiTreeNodeFlags_DefaultOpen)) {
+        ImGui::InputText("Search", _filter.AsChar(), nsString::MAX_SIZE - 1);
+        ImGui::SameLine();
+        if (ImGui::Button("Clear")) {
+            _filter = "";
         }
-    }
-    ImGui::EndChild();
 
-    if (ImGui::Button("Create")) {
-        auto path = _model->GetProjectPath();
-        const auto popup = nsPopupsStack::Shared()->AddPopup<nsOpenFilePopup>(path);
-        popup->SetTitle("Select layout file");
-        popup->SetExtensions( {"layout"});
-        popup->SetOpenCallback([](const nsFilePath &layoutPath) {
-            nsUndoService::Shared()->Push(new nsUndoCreateLayout(layoutPath, new nsVisualContainer2d()));
-        });
+        auto &user = _model->project.user;
+
+        ImGui::BeginChild("LayoutsLib", ImVec2(0, 300),
+                          ImGuiChildFlags_Borders | ImGuiChildFlags_ResizeY,
+                          ImGuiWindowFlags_HorizontalScrollbar);
+        for (auto file: _model->project.scenes.GetFiles()) {
+            if (_filter.IsEmpty() || strstr(file.AsChar(), _filter.AsChar())) {
+                if (ImGui::Selectable(file.AsChar(), user.currentScene == file.AsChar(),
+                                      ImGuiSelectableFlags_AllowDoubleClick)) {
+                    if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
+                        Log::Info("Selected: %s", file.AsChar());
+                        const std::string value = file.AsChar();
+                        const auto undo = nsUndoService::Shared();
+                        const auto batch = new nsUndoBatch();
+
+                        batch->Add(new nsUndoVarChange(user.selectedObject,
+                                                       static_cast<nsVisualObject2d *>(nullptr)));
+                        batch->Add(new nsUndoVarChange(user.currentScene, value));
+
+                        undo->Push(batch);
+                    }
+                }
+
+                if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
+                    const char *data = file.AsChar();
+                    ImGui::SetDragDropPayload("MY_DND_TYPE", data, strlen(data) + 1);
+                    ImGui::Text("Drop to scene tree...");
+                    ImGui::EndDragDropSource();
+                } //*/
+            }
+        }
+        ImGui::EndChild();
+
+        if (ImGui::Button("Create")) {
+            auto path = _model->GetProjectPath();
+            const auto popup = nsPopupsStack::Shared()->AddPopup<nsOpenFilePopup>(path);
+            popup->SetTitle("Select layout file");
+            popup->SetExtensions({"layout"});
+            popup->SetOpenCallback([](const nsFilePath &layoutPath) {
+                nsUndoService::Shared()->Push(new nsUndoCreateLayout(layoutPath, new nsVisualContainer2d()));
+            });
+        }
     }
 
     ImGui::End();
