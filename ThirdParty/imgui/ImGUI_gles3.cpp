ebe600499fdb199e9648bce1d29f8921187aaed9
Roman.Gaikov <gaikov@inbox.ru>
2026-02-11 15:11:05 +0300

    "Enable docking and multi-viewport support in ImGui backends.
    Added support for docking and platform multi-viewport capabilities to the ImGui backends, enhancing user interface flexibility. This includes integration of a 'ShowDockSpace' method, viewport management for multiple windows, and modifications to handle platform-specific multi-viewport features for OSX and other backends."

--- ThirdParty/imgui/ImGUI_gles3.cpp
+++ ThirdParty/imgui/ImGUI_gles3.cpp
@@ -14,15 +14,13 @@
 ImGuiKey ImGui_ImplGlfw_KeyToImGuiKey(int keycode, int scancode);
 
 bool nsImGUI_gles3::Init(void *window) {
-    // Инициализируем ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImPlot::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Включаем клавиатуру
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard
+            | ImGuiConfigFlags_DockingEnable;
 
-
-    // Инициализируем бэкенды ImGui для GLFW и OpenGL ES 3
     ImGui_ImplGlfw_InitForOpenGL(static_cast<GLFWwindow *>(window), false);
     ImGui_ImplOpenGL3_Init("#version 300 es");
     return true;
@@ -41,6 +39,7 @@ void nsImGUI_gles3::StartFrame() {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
+    ShowDockSpace();
 }
 
 void nsImGUI_gles3::EndFrame() {
@@ -52,19 +51,19 @@ void nsImGUI_gles3::EndFrame() {
 }
 
 bool nsImGUI_gles3::OnPointerUp(float x, float y, int pointerId) {
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     io.AddMouseButtonEvent(pointerId, false);
     return io.WantCaptureMouse;
 }
 
 bool nsImGUI_gles3::OnPointerDown(float x, float y, int pointerId) {
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     io.AddMouseButtonEvent(pointerId, true);
     return io.WantCaptureMouse;
 }
 
 bool nsImGUI_gles3::OnPointerMove(float x, float y, int pointerId) {
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     io.AddMousePosEvent(x, y);
     return io.WantCaptureMouse;
 }
@@ -73,13 +72,13 @@ void nsImGUI_gles3::OnPointerCancel(int pointerId) {
 }
 
 void nsImGUI_gles3::OnKeyUp(const int key, const int mods) {
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     io.AddKeyEvent(ImGui_ImplGlfw_KeyToImGuiKey(key, 0), false);
 
-    io.AddKeyEvent(ImGuiMod_Ctrl,(mods & GLFW_MOD_CONTROL) != 0);
-    io.AddKeyEvent(ImGuiMod_Shift,(mods & GLFW_MOD_SHIFT) != 0);
-    io.AddKeyEvent(ImGuiMod_Alt,(mods & GLFW_MOD_ALT) != 0);
-    io.AddKeyEvent(ImGuiMod_Super,(mods & GLFW_MOD_SUPER) != 0);
+    io.AddKeyEvent(ImGuiMod_Ctrl, (mods & GLFW_MOD_CONTROL) != 0);
+    io.AddKeyEvent(ImGuiMod_Shift, (mods & GLFW_MOD_SHIFT) != 0);
+    io.AddKeyEvent(ImGuiMod_Alt, (mods & GLFW_MOD_ALT) != 0);
+    io.AddKeyEvent(ImGuiMod_Super, (mods & GLFW_MOD_SUPER) != 0);
 }
 
 void nsImGUI_gles3::OnKeyDown(const int key, const bool rept, const int mods) {
@@ -87,22 +86,49 @@ void nsImGUI_gles3::OnKeyDown(const int key, const bool rept, const int mods) {
         return;
     }
 
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     io.AddKeyEvent(ImGui_ImplGlfw_KeyToImGuiKey(key, 0), true);
 
-    io.AddKeyEvent(ImGuiMod_Ctrl,(mods & GLFW_MOD_CONTROL) != 0);
-    io.AddKeyEvent(ImGuiMod_Shift,(mods & GLFW_MOD_SHIFT) != 0);
-    io.AddKeyEvent(ImGuiMod_Alt,(mods & GLFW_MOD_ALT) != 0);
-    io.AddKeyEvent(ImGuiMod_Super,(mods & GLFW_MOD_SUPER) != 0);
+    io.AddKeyEvent(ImGuiMod_Ctrl, (mods & GLFW_MOD_CONTROL) != 0);
+    io.AddKeyEvent(ImGuiMod_Shift, (mods & GLFW_MOD_SHIFT) != 0);
+    io.AddKeyEvent(ImGuiMod_Alt, (mods & GLFW_MOD_ALT) != 0);
+    io.AddKeyEvent(ImGuiMod_Super, (mods & GLFW_MOD_SUPER) != 0);
 }
 
 void nsImGUI_gles3::OnChar(char ch) {
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     io.AddInputCharacter(ch);
 }
 
 bool nsImGUI_gles3::OnMouseWheel(float delta) {
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     io.AddMouseWheelEvent(0, delta);
     return io.WantCaptureMouse;
+}
+
+void nsImGUI_gles3::ShowDockSpace() {
+
+    const ImGuiViewport *viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+    ImGui::SetNextWindowViewport(viewport->ID);
+
+    constexpr ImGuiWindowFlags window_flags =
+            ImGuiWindowFlags_NoTitleBar |
+            ImGuiWindowFlags_NoCollapse |
+            ImGuiWindowFlags_NoResize |
+            ImGuiWindowFlags_NoMove |
+            ImGuiWindowFlags_NoDocking |
+            ImGuiWindowFlags_NoBringToFrontOnFocus |
+            ImGuiWindowFlags_NoNavFocus |
+            ImGuiWindowFlags_NoBackground;
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::Begin("DockSpace Demo", nullptr, window_flags);
+
+    const ImGuiID spaceId = ImGui::GetID("MyDockSpace");
+    ImGui::DockSpace(spaceId, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
+
+    ImGui::End();
+    ImGui::PopStyleVar();
 }
