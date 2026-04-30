4a31db05816218e248d651bf18edcb5c74954b8c
Roman.Gaikov <gaikov@inbox.ru>
2026-04-30 19:40:54 +0300

    MovieClip implementation

--- Engine/display/movieClip/MovieClipBuilder.cpp
+++ Engine/display/movieClip/MovieClipBuilder.cpp
@@ -8,12 +8,12 @@
 #include "Core/ParserUtils.h"
 #include "RenManager.h"
 
-nsVisualObject2d* nsMovieClipBuilder::CreateDefault() {
+nsVisualObject2d *nsMovieClipBuilder::CreateDefault() {
     return new nsMovieClip();
 }
 
 bool nsMovieClipBuilder::Parse(script_state_t *ss, nsVisualObject2d *object, nsVisualCreationContext2d *context) {
-    auto *mc = dynamic_cast<nsMovieClip*>(object);
+    auto *mc = dynamic_cast<nsMovieClip *>(object);
     if (!mc) return false;
 
     if (!nsVisualBuilder2d::Parse(ss, object, context)) return false;
@@ -28,56 +28,54 @@ bool nsMovieClipBuilder::Parse(script_state_t *ss, nsVisualObject2d *object, nsV
         }
     }
 
-    while (ps_var_begin(ss, "anim")) {
-        nsAnimClip clip;
+    if (ps_block_begin(ss, "anim")) {
+        do {
+            nsAnimClip clip;
 
-        if (ps_var_begin(ss, "name")) {
-            clip.name = ps_var_str(ss);
-            ps_var_next(ss);
-        }
-
-        if (ps_var_begin(ss, "fps")) {
-            clip.fps = (float)ps_var_f(ss);
-            ps_var_next(ss);
-        }
-
-        while (ps_var_begin(ss, "frame")) {
-            nsSpriteDesc desc;
-
-            assetPath = context->assetsContext->ParseAssetPath(ss, "texture");
-            if (!assetPath.IsEmpty()) {
-                if (const auto tex = dev->TextureLoad(assetPath, false)) {
-                    desc.tex = tex;
-                    desc.ResetSize();
-                }
+            if (ps_var_begin(ss, "name")) {
+                clip.name = ps_var_str(ss);
             }
 
-            if (ps_var_begin(ss, "tex1")) {
-                ps_var_2f(ss, desc.tex1);
-                ps_var_next(ss);
+            if (ps_var_begin(ss, "fps")) {
+                clip.fps = (float) ps_var_f(ss);
             }
 
-            if (ps_var_begin(ss, "tex2")) {
-                ps_var_2f(ss, desc.tex2);
-                ps_var_next(ss);
+            if (ps_block_begin(ss, "frame")) {
+                do {
+                    nsSpriteDesc desc;
+
+                    assetPath = context->assetsContext->ParseAssetPath(ss, "texture");
+                    if (!assetPath.IsEmpty()) {
+                        if (const auto tex = dev->TextureLoad(assetPath, false)) {
+                            desc.tex = tex;
+                            desc.ResetSize();
+                        }
+                    }
+
+                    if (ps_var_begin(ss, "tex1")) {
+                        ps_var_2f(ss, desc.tex1);
+                    }
+
+                    if (ps_var_begin(ss, "tex2")) {
+                        ps_var_2f(ss, desc.tex2);
+                    }
+
+                    if (ps_var_begin(ss, "size")) {
+                        ps_var_2f(ss, desc.size);
+                    }
+
+                    if (ps_var_begin(ss, "pivot")) {
+                        ps_var_2f(ss, desc.center);
+                    }
+
+                    clip.frames.push_back(desc);
+                } while (ps_block_next(ss));
+                ps_block_end(ss);
             }
 
-            if (ps_var_begin(ss, "size")) {
-                ps_var_2f(ss, desc.size);
-                ps_var_next(ss);
-            }
-
-            if (ps_var_begin(ss, "pivot")) {
-                ps_var_2f(ss, desc.center);
-                ps_var_next(ss);
-            }
-
-            clip.frames.push_back(desc);
-            ps_var_next(ss);
-        }
-
-        mc->AddAnim(clip);
-        ps_var_next(ss);
+            mc->AddAnim(clip);
+        } while (ps_block_next(ss));
+        ps_block_end(ss);
     }
 
     return true;
